078691fc017200850b854a602b4af0d4822ab459
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-04-19 23:03:30 +0530

    hero section completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AiFillTag, AiOutlineClose, AiOutlineMenu, AiOutlineSearch } from 'react-icons/ai'
 import { BsFillCartFill, BsFillSaveFill } from 'react-icons/bs'
 import { FaUserFriends } from 'react-icons/fa'
@@ -7,10 +7,12 @@ import { MdFavorite, MdHelp } from 'react-icons/md'
 import { TbTruckDelivery } from 'react-icons/tb'
 
 const Navbar = () => {
+
+    const [nav, setNav] = useState(false)
   return (
     <div className='max-w-[1640px] mx-auto flex justify-between items-center p-4'>
         <div className='flex item-center'>
-            <div className='cursor-pointer'>
+            <div className='cursor-pointer' onClick={() => setNav(!nav)}>
                 <AiOutlineMenu size={30} />
             </div>
             <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>Best <span className='font-bold'>Eats</span></h1>
@@ -31,11 +33,12 @@ const Navbar = () => {
 
         {/*mobile menu*/}
         {/*overlay*/}
-        <div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0'></div>
+        {nav ? <div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0'></div> : ''}
+        
 
         {/*side drawer menu*/}
-        <div className='fixed top-0 left-0  w-[300px] h-screen bg-white z-10 duration-300'>
-            <AiOutlineClose size={30} className='absolute right-4 top-4 cursor-pointer'/>
+        <div className={nav ? 'fixed top-0 left-0  w-[300px] h-screen bg-white z-10 duration-300': 'fixed top-0 left-[-100%]  w-[300px] h-screen bg-white z-10 duration-300' }>
+            <AiOutlineClose onClick={() => setNav(!nav)} size={30} className='absolute right-4 top-4 cursor-pointer'/>
             <h2 className='text-2xl p-4'>Best <span className='font-bold'>Eats</span></h2>
             <nav>
                 <ul className='flex flex-col p-4 text-gray-800'>
