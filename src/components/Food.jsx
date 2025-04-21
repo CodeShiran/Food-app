39eae8b2321dee6b7b156c6281aecd0db0f1611e
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-04-21 11:19:37 +0530

    food grid completed

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {data} from '../data/data'
 
 const Food = () => {
+
+    const [foods, setFoods] = useState(data)
   return (
     <div className='max-w-[1640px] m-auto px-4 py-12 '>
         <h1 className='text-orange-600 font-bold text-4xl text-center'>Top Rated Menu Items</h1>
@@ -25,11 +27,27 @@ const Food = () => {
                 <p className='font-bold text-gray-700'>Filter Price</p>
                 <div className='flex justify-between max-w-[390px] w-full'>
                     <button className='border-orange-600 text-orange hover: bg-orange-600 hover:text-white m-1'>$</button>
-                    <button className='border-orange-600 text-orange hover: bg-orange-600 hover:text-white m-1'>$5</button>
-                    <button className='border-orange-600 text-orange hover: bg-orange-600 hover:text-white m-1'>$55</button>
-                    <button className='border-orange-600 text-orange hover: bg-orange-600 hover:text-white m-1'>$555</button>
+                    <button className='border-orange-600 text-orange hover: bg-orange-600 hover:text-white m-1'>$$</button>
+                    <button className='border-orange-600 text-orange hover: bg-orange-600 hover:text-white m-1'>$$$</button>
+                    <button className='border-orange-600 text-orange hover: bg-orange-600 hover:text-white m-1'>$$$$</button>
                 </div>
             </div>
+        </div>
+        {/*display fruits*/}
+        <div className='grid grid-cols-2 lg:grid-cols-4 gap-6'>
+            {
+                foods.map((item, index) => (
+                    <div key={index} className='border shadow-lg hover:scale-105 duration-300 rounded-lg'>
+                        <img src={item.image} alt={item.name} className='w-full h-[200px] object-cover rounded-t-lg'/>
+                        <div className='flex justify-between px-2 py-4'>
+                            <p className='font-bold'>{item.name}</p>
+                            <p>
+                                <span className='bg-orange-500 text-white p-1'>{item.price}</span>
+                            </p>
+                        </div>
+                    </div>
+                ))
+            }
         </div>
     </div>
   )
